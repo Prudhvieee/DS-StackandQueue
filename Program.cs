6962ef34d5867d3e885bbde24b4663f7c68a6f50
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-08 23:26:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_StackandQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures Stack and Queue");
+        }
+    }
+}
