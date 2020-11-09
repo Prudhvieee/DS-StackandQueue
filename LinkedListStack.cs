2f9dfe295aba517760e834cde551f82106158302
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-09 07:06:26 +0530

    [Prudhvi] Add . Creating stack using linked list

---
+++ LinkedListStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_StackandQueue
+{
+    public class LinkedListStack
+    {
+        Node top;
+        /// <summary>
+        /// Pushes the specified data into the stack.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+            Console.WriteLine(node.data + " pushed to Stack");
+        }
+        /// <summary>
+        /// Displays the elements present in the stack.
+        /// </summary>
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node node = top;
+                while (node.next != null)
+                {
+                    Console.Write(node.data + " ");
+                    node = node.next;
+                }
+                Console.Write(node.data + "\n");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace DS_StackandQueue
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structures Stack and Queue");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
